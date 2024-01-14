93dea30b71b694bde596de702a4791639d8778dd
Tamasha Thiruni <thirunitamasha@gmail.com>
2024-01-14 16:14:03 +0530

    first commit

---
+++ resources/views/FrontEnds/tasks.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="text-center">
+            <h2>Daily Tasks</h2>
+            <input type="text" class="form-control" placeholder="Enter Your Task Here">
+            <br>
+            <input type="button" class="btn btn-primary" value="Save">
+            <input type="button" class="btn btn-warning" value="Clear">
+            <br>
+            <table class="table table-dark">
+                
+            </table>
+        </div>
+        <!--<div class="img">
+            <img src="/image/Screenshot.png" alt="">
+        </div> -->
+    </div>
+</body>
+</html>
